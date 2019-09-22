db2875e246e2596cd5589117dcb60e71337ecf7b
Andrew2432 <drew.devops@gmail.com>
2019-09-22 08:55:30 +0530

    Minor change in path v3

--- dist/js/app.js
+++ dist/js/app.js
@@ -16,12 +16,12 @@ function getSelectedMap() {
 
 	new_year = String(year);
 	new_month = String(month);
-	new_year_short = String(new_year).substring(2, 4);  
-	base_address = "water_data\\img\\";
-	new_address = `${base_address}${new_year}\\avwl_${new_month}${new_year_short}.png`;
-  
-  console.log(new_address);
-	
-  imgTag = document.getElementById("map");
+	new_year_short = String(new_year).substring(2, 4);
+	base_address = "water_data/img/";
+	new_address = `${base_address}${new_year}/avwl_${new_month}${new_year_short}.png`;
+
+	console.log(new_address);
+
+	imgTag = document.getElementById("map");
 	document.getElementById("map").setAttribute("src", new_address);
 }
