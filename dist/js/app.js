73b3e750ef50361bd3af6f8f5ab9c1152591e4ec
Andrew2432 <drew.devops@gmail.com>
2019-09-22 11:47:54 +0530

    Correcred path in JS

--- dist/js/app.js
+++ dist/js/app.js
@@ -38,8 +38,8 @@ function getSelectedMap() {
 	new_csv_address = `${base_csv_address}${new_year}/avwl_${new_month}${new_year_short}.csv`;
 
 	// Check if image file exists, else use default file
-	url_new_address = `http://127.0.0.1:5501/${new_address}`;
-	//url_new_address = `https://tnwatermap.netlify.com/${new_address}`;
+	//url_new_address = `http://127.0.0.1:5501/${new_address}`;
+	url_new_address = `https://tnwatermap.netlify.com/${new_address}`;
 	isURL = doesFileExist(url_new_address);
 
 	if (isURL) {
